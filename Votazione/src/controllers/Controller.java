24c6871c278b68540bbd56de21a627262d3d744b
marco <marco.ottaiano@studenti.unimi.it>
2022-03-08 12:35:24 +0100

    completato registrazione, trovato loop che va risolto

--- Votazione/src/controllers/Controller.java
+++ Votazione/src/controllers/Controller.java
@@ -196,7 +196,7 @@ public class Controller implements UserDao{
 		}
     }
     
-    public String MD5(String md5) {
+    public static String MD5(String md5) {
     	   try {
     	        java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
     	        byte[] array = md.digest(md5.getBytes());
--- Votazione/src/controllers/RegController.java
+++ Votazione/src/controllers/RegController.java
@@ -5,16 +5,29 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import dao.UserDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.util.Pair;
+import javafx.scene.control.Alert.AlertType;
 import model.Elettore;
 import model.User;
 
@@ -67,27 +80,146 @@ public class RegController implements UserDao{
 
     @FXML
     void handleReg(ActionEvent event) throws IOException {
+    	LocalDate currentDate = LocalDate.now();
     	
-    	//Elettore e = new Elettore(lblNome.getText(), lblCognome.getText(), lblCodFiscale.getText(), lblDataNascita.getAccessibleText(), lblComuneNascita.getText(), lblNazioneNascita.getText(), lblSesso.getText());
-    	
-    	//se rispetta tutte le condizioni per la creazione dell'elettore
-    	//charge(e);
-    	
-    	Main m = new Main();
-    	m.changeScene("../gui/main.fxml");
+    	String sesso="";
+    	if (lblUomo.isSelected()) {
+    		sesso = "m";
+    	}else if (lblDonna.isSelected()) {
+    		sesso = "f";
+    	}
+    	if((lblNome.getText().isBlank())||(lblCognome.getText().isBlank())||(lblCodFiscale.getText().isBlank())||(lblDataNascita.getValue().toString().isBlank())||(lblComuneNascita.getText().isBlank())||(lblNazioneNascita.getText().isBlank())||(lblNome.getText().isBlank())||(sesso.isBlank())) {
+    		Alert alert= new Alert(AlertType.ERROR);
+    		alert.setHeaderText("Registrazione incompleta");
+    		alert.setContentText("Si prega di inserire tutte le informazioni richieste.");
+    		alert.showAndWait();
+    	}else {
+    		if((currentDate.getYear()-lblDataNascita.getValue().getYear())<18) {
+    			Alert alert= new Alert(AlertType.ERROR);
+        		alert.setHeaderText("Utente minorenne");
+        		alert.setContentText("Mi dispiace ma è necessario avere almeno 18 anni per registrarsi.");
+        		alert.showAndWait();
+    			
+    		}else {
+    			if((currentDate.getYear()-lblDataNascita.getValue().getYear())==18) {
+    				if(currentDate.getMonthValue()<lblDataNascita.getValue().getMonthValue()) {
+    					Alert alert= new Alert(AlertType.ERROR);
+    	        		alert.setHeaderText("Utente minorenne");
+    	        		alert.setContentText("Mi dispiace ma è necessario avere almeno 18 anni per registrarsi.");
+    	        		alert.showAndWait();
+    				} else {
+    					if(currentDate.getMonthValue()==lblDataNascita.getValue().getMonthValue()) {
+    						if(currentDate.getDayOfMonth()<lblDataNascita.getValue().getDayOfMonth()) {
+    							Alert alert= new Alert(AlertType.ERROR);
+    	    	        		alert.setHeaderText("Utente minorenne");
+    	    	        		alert.setContentText("Mi dispiace ma è necessario avere almeno 18 anni per registrarsi.");
+    	    	        		alert.showAndWait();
+        					} else {
+        						Elettore e = new Elettore(lblNome.getText(), lblCognome.getText(), lblCodFiscale.getText(), lblDataNascita.getValue().toString(), lblComuneNascita.getText(), lblNazioneNascita.getText(),sesso);
+        				    	
+        		    			//se rispetta tutte le condizioni per la creazione dell'elettore
+        		    			charge(e);
+        					}
+    					} else {
+	    					Elettore e = new Elettore(lblNome.getText(), lblCognome.getText(), lblCodFiscale.getText(), lblDataNascita.getValue().toString(), lblComuneNascita.getText(), lblNazioneNascita.getText(),sesso);
+	    			    	
+	    	    			//se rispetta tutte le condizioni per la creazione dell'elettore
+	    	    			charge(e);
+    					}
+    				} 
+    			} else {
+    				Elettore e = new Elettore(lblNome.getText(), lblCognome.getText(), lblCodFiscale.getText(), lblDataNascita.getValue().toString(), lblComuneNascita.getText(), lblNazioneNascita.getText(),sesso);
+    		    	
+        			//se rispetta tutte le condizioni per la creazione dell'elettore
+        			charge(e);
+    			}
+    		}
+    		
+    	}
     }
     
     private void charge(Elettore e) throws IOException{
-		String sql = "INSERT INTO `dbvotazione`.`userdata` (`nome`, `cognome`, `codfiscale`, `datan`, `comunen`, `nazionen`, `sesso`, `voto`) VALUES ( '" + e.nome + "' , '" + e.cognome + "' ,'" + e.codfiscale + "','" + e.datan + "', '" + e.comunen + "' , '" + e.nazionen + "' , '" + e.sesso + "' , " + e.voto+ ")";
-		
-			try(Connection conn = DriverManager.getConnection(DBADDRESS, USER, PWD);
-					PreparedStatement pr = conn.prepareStatement(sql);
-						){
-					int r = pr.executeUpdate(sql);
-				} catch(Exception e1) {
-					e1.printStackTrace();
-				}
-    
+    	boolean a = true;
+    	while(a) {
+	    	// Create the custom dialog.
+	    	Dialog<Pair<String, String>> dialog = new Dialog<>();
+	    	dialog.setTitle("Inserimento password");
+	    	dialog.setHeaderText("Si prega di inserire la password");
+	    	
+	    	// Set the button types.
+	    	ButtonType loginButtonType = new ButtonType("Conferma", ButtonData.OK_DONE);
+	    	dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+	    	
+	    	// Create the username and password labels and fields.
+	    	GridPane grid = new GridPane();
+	    	grid.setHgap(10);
+	    	grid.setVgap(10);
+	    	grid.setPadding(new Insets(20, 150, 10, 10));
+	
+	    	PasswordField password = new PasswordField();
+	    	password.setPromptText("Password");
+	    	PasswordField confpassword = new PasswordField();
+	    	confpassword.setPromptText("Conferma password");
+	
+	    	grid.add(new Label("Password:"), 0, 0);
+	    	grid.add(password, 1, 0);
+	    	grid.add(new Label("Conferma Password:"), 0, 1);
+	    	grid.add(confpassword, 1, 1);
+	    	
+	    	dialog.getDialogPane().setContent(grid);
+	    	
+	    	// Convert the result to a username-password-pair when the login button is clicked.
+	    	dialog.setResultConverter(dialogButton -> {
+	    	    if (dialogButton == loginButtonType) {
+	    	        return new Pair<>(password.getText(), confpassword.getText());
+	    	    }
+	    	    return null;
+	    	});
+	    	
+			Optional<Pair<String, String>> result = dialog.showAndWait();
+			if(result.isPresent()) {
+	    	if (!((password.getText().isBlank())||(confpassword.getText().isBlank()))){
+	    		if(confpassword.getText().equals(password.getText())) {   
+	    			String sql = "INSERT INTO `dbvotazione`.`userdata` (`nome`, `cognome`, `codfiscale`, `datan`, `comunen`, `nazionen`, `sesso`, `voto`) VALUES ( '" + e.nome + "' , '" + e.cognome + "' ,'" + e.codfiscale + "','" + e.datan + "', '" + e.comunen + "' , '" + e.nazionen + "' , '" + e.sesso + "' , " + e.voto+ ")";
+	    			
+	    			try(Connection conn = DriverManager.getConnection(DBADDRESS, USER, PWD);
+	    					PreparedStatement pr = conn.prepareStatement(sql);
+	    						){
+	    					int r = pr.executeUpdate(sql);
+	    				} catch(Exception e1) {
+	    					e1.printStackTrace();
+	    			}
+	    			String sql2 = "INSERT INTO `dbvotazione`.`credentials` (`username`, `password`, `amm`) VALUES ( '" + e.codfiscale + "' , '" + Controller.MD5(password.getText()) + "','0')";
+	    			
+	    			try(Connection conn = DriverManager.getConnection(DBADDRESS, USER, PWD);
+	    					PreparedStatement pr = conn.prepareStatement(sql2);
+	    						){
+	    					int r = pr.executeUpdate(sql2);
+	    				} catch(Exception e1) {
+	    					e1.printStackTrace();
+	    				}
+	    			a = false;
+	    			Alert alert= new Alert(AlertType.INFORMATION);
+	        		alert.setHeaderText("Registrazione completata");
+	        		alert.setContentText("Grazie e buona giornata!");
+	        		alert.showAndWait();
+	    	    	Main m = new Main();
+	    	    	m.changeScene("../gui/main.fxml");
+	    	    	
+	    		} else {
+	    			Alert alert= new Alert(AlertType.ERROR);
+	        		alert.setHeaderText("Conferma password errata");
+	        		alert.setContentText("Le due password sono diverse!");
+	        		alert.showAndWait();
+	    		}
+	    	} else {
+	    		Alert alert= new Alert(AlertType.ERROR);
+	    		alert.setHeaderText("Registrazione incompleta");
+	    		alert.setContentText("Si prega di inserire una password!");
+	    		alert.showAndWait();
+	    	}
+    	}
+    	}
     }
     
     @FXML
